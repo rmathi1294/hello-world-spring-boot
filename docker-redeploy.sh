a48b4d102eb79a07f514fe347c60939c847a4291
rmathi1294 <62194685+rmathi1294@users.noreply.github.com>
2022-10-30 00:06:57 +0530

    Update docker-redeploy.sh

--- docker-redeploy.sh
+++ docker-redeploy.sh
@@ -3,3 +3,5 @@ docker pull rmathi/hello-world-spring-boot:latest
 docker rm -f hello-world
 docker container prune -f
 docker run -d --name hello-world -p 8080:8080 rmathi/hello-world-spring-boot:latest
+docker ps
+curl http://199.241.139.163:8080/
